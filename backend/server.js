fd11873fad3ee25a797dc4597c4466dd04d7404e
Kesar.Bhimani <kesar.bhimani@diamnow.com>
2022-11-09 23:04:08 +0530

    Connect with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db")
+
+connectDB() 
 
 const port = process.env.PORT || 5000;
 
