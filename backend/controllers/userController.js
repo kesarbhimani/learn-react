4f511b9cade833373ae28f691c831e7e4454403d
“kesarbhimani” <kesar.bhimani@gmail.com>
2023-02-09 00:13:36 +0530

    create frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
 
 //@desc  Register new user
-//@route PUT/api/users
+//@route POST /api/users
 //@access Public    
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
