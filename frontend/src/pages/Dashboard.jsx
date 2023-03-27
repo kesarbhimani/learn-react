5eea222f2c12790abdc40f3d7e5a8682092cefae
“kesarbhimani” <kesar.bhimani@gmail.com>
2023-03-28 00:21:20 +0530

    Display Goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
 import Spinner from "../components/Spinner";
 import { getGoals, reset } from "../features/goals/goalSlice";
+import GoalItem from "../components/GoalItem";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -29,17 +30,30 @@ function Dashboard() {
     };
   }, [user, navigate, isError, message, dispatch]);
 
-  // if (isLoading) {
-  //   return <Spinner />;
-  // }
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
       <section className="heading">
-        <h1 className="font-mono text-blue-700">Welcome {user && user.name}</h1>
-        <p className="font-mono text-xl text-blue-500">Goals Dashboard</p>
+        <h1 className="text-blue-700">Welcome {user && user.name}</h1>
+        <p className="text-lg text-blue-500 font-medium">Goals Dashboard</p>
       </section>
+
       <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   );
 }
